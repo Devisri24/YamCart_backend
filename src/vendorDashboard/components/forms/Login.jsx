b8db23b1a095c6ba5c4166d6da759e847fece122
Devisri <yennidevisri13.7@gmail.com>
2025-06-24 22:58:28 +0530

    Fix: Delay reload to set firm name after login

--- src/vendorDashboard/components/forms/Login.jsx
+++ src/vendorDashboard/components/forms/Login.jsx
@@ -37,13 +37,14 @@ const Login = ({showWelcomeHandler}) => {
           const vendorId = data.vendorId
           console.log("checking for VendorId:",vendorId)
           const vendorResponse = await fetch(`${API_URL}/vendor/single-vendor/${vendorId}`)
-          window.location.reload()
           const vendorData = await vendorResponse.json();
           if(vendorResponse.ok){
             const vendorFirmId = vendorData.vendorFirmId;
             const vendorFirmName = vendorData.vendor.firm[0].firmName;
             localStorage.setItem('firmId', vendorFirmId);
             localStorage.setItem('firmName', vendorFirmName)
+            window.location.reload()
+
           }
       } catch (error) {
           alert("login fail")
